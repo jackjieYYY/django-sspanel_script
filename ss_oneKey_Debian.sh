6f6b4d259c6cd8f5a5341dad256b854dd828e243
jackjieYYY <53422254+jackjieYYY@users.noreply.github.com>
2019-10-29 13:58:51 +1100

    Update ss_oneKey_Debian.sh

--- ss_oneKey_Debian.sh
+++ ss_oneKey_Debian.sh
@@ -4,17 +4,15 @@ Token=admin
 APIinit(){
 echo "请输入节点ID"
 read NodeId
-API="\"http:\/\/$Domain\/api\/user_ss_config\/$NodeId\/?token=$Token\""
 }
 APIinit
-echo $API
 apt install -y python3-distutils curl wget git screen
 git clone https://github.com/Ehco1996/aioshadowsocks.git
 cd aioshadowsocks
 wget https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py
 pip3 install -r requirements.txt
-export SS_API_ENDPOINT="http://$Domain/api/user_ss_config/$NodeId/?token=$Token\"
+export SS_API_ENDPOINT="http://$Domain/api/user_ss_config/$NodeId/?token=$Token"
 screen -S sS
 python3 server.py
 echo -e '\004'
