63bf45bf1c8d25d48a049b4326320811aaf28878
jackjieYYY <53422254+jackjieYYY@users.noreply.github.com>
2019-11-22 05:15:30 +1100

    Update ss_oneKey_Debian.sh

--- ss_oneKey_Debian.sh
+++ ss_oneKey_Debian.sh
@@ -1,18 +1,69 @@
 #!/bin/bash
-Domain=nai-ve.com
-Token=admin
+clear
+echo
+echo "#################################################################"
+echo "django-sspanel ss 一键后端"
+echo
+echo "System Required: CentOS Debian"
+echo "#################################################################"
+echo
+
+system_check(){
+	if [ -f /usr/bin/yum ]; then
+		centos_install
+	elif [ -f /usr/bin/apt ]; then
+		debian_install
+	else
+		echo -e "你的系统不支持"
+	fi
+}
+
 APIinit(){
+echo "请输入面板地址(Example:sspanel.com)"
+read Domain
+echo "请输入Token"
+read Token
 echo "请输入节点ID"
 read NodeId
+API="\"http:\/\/$Domain\/api\/user_ss_config\/$NodeId\/?token=$Token\""
+echo "API = http://$Domain/api/user_ss_config/$NodeId/?token=$Token"
+echo "按任意键开始安装"
+read A
 }
-APIinit
-apt install -y python3-distutils curl wget git screen
-git clone https://github.com/Ehco1996/aioshadowsocks.git
-cd aioshadowsocks
-wget https://bootstrap.pypa.io/get-pip.py
-python3 get-pip.py
-pip3 install -r requirements.txt
-export SS_API_ENDPOINT="http://$Domain/api/user_ss_config/$NodeId/?token=$Token"
-screen -S sS
-python3 server.py
-echo -e '\004'
+prepare_Centos(){
+    yum install -y curl git
+}
+prepare_Debian(){
+    apt install -y curl git
+}
+Install(){
+    curl -sSL https://get.docker.com/ | sh
+    curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+    git clone https://github.com/Ehco1996/aioshadowsocks.git
+    cd aioshadowsocks
+    systemctl restart docker
+    sed -i 's/SS_API_ENDPOINT: \"\"/SS_API_ENDPOINT: '"$API"'/g' docker-compose.yml
+    docker-compose up -d
+    chkconfig docker on
+}
+End(){
+echo "安装完毕 请打开防火墙 1000-2000 端口"
+echo "请按任意键退出脚本"
+read A
+}
+centos_install(){
+    APIinit
+    prepare_Centos
+    Install
+}
+
+debian_install(){
+    APIinit
+    prepare_Debian
+    Install
+}
+system_check
+
+
