27a262253adbb2d65bf0b1950303d73135c90a05
jackjieYYY <53422254+jackjieYYY@users.noreply.github.com>
2020-03-21 04:42:45 +1100

    Update v2ray_onekey.sh

--- v2ray_onekey.sh
+++ v2ray_onekey.sh
@@ -47,7 +47,7 @@ install(){
     curl -sSL https://get.docker.com/ | sh
     curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
-    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+    ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
     systemctl enable docker
     systemctl enable docker.service
     systemctl restart docker
