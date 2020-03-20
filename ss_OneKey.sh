99ce69b88e1602e2ef1ecbd75fb1a06dd49a0392
jackjieYYY <53422254+jackjieYYY@users.noreply.github.com>
2020-03-21 04:39:50 +1100

    Update ss_OneKey.sh

--- ss_OneKey.sh
+++ ss_OneKey.sh
@@ -45,7 +45,7 @@ Install(){
     curl -sSL https://get.docker.com/ | sh
     curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
-    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+    ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
     git clone https://github.com/Ehco1996/aioshadowsocks.git
     cd aioshadowsocks
     systemctl restart docker
